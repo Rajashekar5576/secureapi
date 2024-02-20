223c7b53e9a93b0fec45f1eb8fc9366e8816b4b9
Rajashekar reddy <64459091+Rajashekar5576@users.noreply.github.com>
2024-02-20 12:39:45 +0530

    Update app.js
    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
 
